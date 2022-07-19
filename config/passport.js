fcb03e3b04ced6947704e9fbcc3d4413dbe5788f
filipe-alves-riotinto <filipe.alves.riotinto@gmail.com>
2022-07-19 16:02:42 -0300

    Sem erro

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const { authSecret } = require('../.env')
-const passport = require ('passport')
-const passportJwt = require ('passport-jwt')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
 const { Strategy, ExtractJwt } = passportJwt
 
 module.exports = app => {
@@ -8,26 +8,25 @@ module.exports = app => {
         secretOrKey: authSecret,
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     }
-
+    
     const strategy = new Strategy(params, (payload, done) => {
         app.db('users')
-            .where({id: payload})
+            .where({ id: payload.id })
             .first()
             .then(user => {
                 if (user) {
-                    done(null, {id: user.id, email: user.email})
+                    done(null, { id: user.id, email: user.email })
                 } else {
                     done(null, false)
                 }
             })
-            .catch(err => done(err,false))
+            .catch(err => done(err, false))
     })
 
     passport.use(strategy)
 
     return {
         initialize: () => passport.initialize(),
-        authenticate: () => passport.authenticate('jwt', {session: false})
+        authenticate: () => passport.authenticate('jwt', { session: false }),
     }
-
 }
--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,15 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-  client: 'postgresql',
-  connection: {
-    database: 'tasks',
-    user:     'postgres',
-    password: '751953'
-  },
-  pool: {     min: 0,     max: 5,     acquireTimeoutMillis: 60000,     idleTimeoutMillis: 600000, },
- // debug: true,
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-
-
+	client: 'postgresql',
+	connection: {
+		database: 'tasks',
+		user: 'postgres',
+		password: '751953'
+	},
+	pool: {
+		min: 2,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
 };
